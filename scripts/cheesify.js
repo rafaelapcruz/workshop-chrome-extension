114d90f1d1a303b69d71aba7302e79334a4bbf77
rafaelapcruz <79165115+rafaelapcruz@users.noreply.github.com>
2021-09-22 20:36:12 +0100

    build a chrome extension to cheesify web pages
    This chrome extension was built during Le Wagon's workshop during JavaScript Week. It transforms the images from a web page into images related to cheese. Only images that are loaded can be changed.

--- scripts/cheesify.js
+++ scripts/cheesify.js
@@ -1,6 +1,19 @@
 function cheesify() {
   // TODO: Add the image replacement script here
+  const images = document.querySelectorAll('img');
+  images.forEach( (img) => {
+    img.src = `https://source.unsplash.com/featured/?cheese/${img.width}x${img.height}?${Math.random()}`;
+    img.srcset = img.src;
+  })
 }
 
 // TODO: Write a function to listen for messages on the content page using chrome.runtime.onMessage
-  
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // actions based on the request (which corresponds to the object we sent in our message)
+    // console.log(request);
+    if (request.action === 'cheesify') {
+      cheesify();
+    }
+  }
+);
